bac0512bca5cc1a8f40414b0467311bffdcadf50
Nakum Darshan <91113430+darshannakum15@users.noreply.github.com>
2021-11-25 09:08:39 +0530

    Update logicalopr.c

--- logicalopr.c
+++ logicalopr.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-
 int main()
 {
-	int a=5,b=3;
+    int a = 5, b = 5, c = 10, result;
 
-    printf("%d==%d is %d\n",a,b,a==b);
-    printf("%d>%d is %d\n",a,b,a>b);
-    printf("%d<%d is %d\n",a,b,a<b);
-    printf("%d!=%d is %d\n",a,b,a!=b);
-    printf("%d<=%d is %d\n",a,b,a<=b);
-    printf("%d>=%d is %d",a,b,a>=b);
+    result = (a == b) && (c > b);
+    printf("(a == b) && (c > b) is %d \n", result);
+
+    result = (a == b) && (c < b);
+    printf("(a == b) && (c < b) is %d \n", result);
+
+    result = (a == b) || (c < b);
+    printf("(a == b) || (c < b) is %d \n", result);
+
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);
 
     return 0;
 }
