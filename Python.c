2f2a0f3d8e06940a44afee5699c86df818ea30de
Nakum Darshan <91113430+darshannakum15@users.noreply.github.com>
2021-12-10 21:12:59 +0530

    Add files via upload

---
+++ Python.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+#include<time.h>
+
+int main()
+{
+    time_t t;   // not a primitive datatype
+    time(&t);
+    printf("Date and Time :- %s", ctime(&t));
+
+    return 0;
+}
---
+++ gets.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>  
+int main ()  
+{  
+    char s[30];  
+    printf("Enter the string");
+	gets(s);
+   
+ printf("You Entered %s",s);
+	puts(s);
+	return 0; 
+}
