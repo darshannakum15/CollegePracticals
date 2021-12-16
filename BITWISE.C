7d6a049f7b01aac0e9e89e2867a909de528210be
Nakum Darshan <91113430+darshannakum15@users.noreply.github.com>
2021-12-16 18:15:23 +0530

    Add files via upload

---
+++ BITWISE.C
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+	clrscr();
+	char a= 5, b=9;
+	printf("a= %d, b = %d\n",a,b);
+	printf("a&b = %d\n", a & b);
+	printf("a|b = %d\n", a | b);
+	printf("a^b = %d\n", a ^ b);
+	printf("~a = %d\n", a = ~b);
+	printf("b<<1 = %d\n", b<<1);
+	getch();
+}
